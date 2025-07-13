f6d9fea4879aca71ef07064bfa5cd41710466cbd
殉道者是也 <1160793279@qq.com>
2025-07-13 13:47:04 +0800

    删除了冗余的test文件夹与log_manger文件夹（因为该部分已合并到IDS系统中），若需要获取以前的代码，可自行拉取以下版本号对应的代码：5255147d128563d7822df19bf073b6ee514e6e79

--- src/log_manager/Database Schema/http_session.sql
+++
@@ -1,8 +0,0 @@
--- HTTP会话表
-CREATE TABLE `http_session` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `client_ip` varchar(45) NOT NULL,
-  `start_time` datetime NOT NULL,
-  `end_time` datetime DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4; 
